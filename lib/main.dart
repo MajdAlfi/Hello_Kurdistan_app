a11c71c268345808024dde417eb8d59d0e6e69c8
“Majd <majdsomer@gmail.com>
2022-06-19 01:28:23 +0300

    Adding a curved kudistan flag, some text styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +11,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(body: Center(child: Text("Hello Kurdistan"))));
+        home: Scaffold(
+            body: Center(
+                child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+              width: 300,
+              height: 170,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Flag_of_Kurdistan.svg/1200px-Flag_of_Kurdistan.svg.png"),
+                      fit: BoxFit.fill),
+                  borderRadius: BorderRadius.all(Radius.circular(20)))),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Text(
+            "Hello Kurdistan",
+            style: TextStyle(
+                fontFamily: "Roboto",
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3,
+                fontSize: 25),
+          ),
+        )
+      ],
+    ))));
   }
 }
